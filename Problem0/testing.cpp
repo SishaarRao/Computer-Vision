adbd4cfdf7c241c43a1bd34161cbef3ffc4b3c20
Sishaar Rao <sishaar@gmail.com>
2017-02-08 15:30:59 -0500

    implemented distance function

--- Problem0/testing.cpp
+++ Problem0/testing.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
+#include <cmath>
+
 using namespace std;
 
+float distance(float x1, float y1, float x2, float y2) {
+   return sqrt(pow((x2 - x1),2) + pow((y2 - y1),2));
+}
+
 int main() {
-   cout << "Hello World";
+   cout << distance(0,0,1,1);
+
+
    return 0;
 }
